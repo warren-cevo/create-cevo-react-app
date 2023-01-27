f48d39c6a42da81b256ab121039c20f9c670e9f7
WarrenCevo <warren.abundo@cevo.com.au>
2023-01-27 17:30:33 +1100

    Add amplify config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
     plugins: [react()],
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
     test: {
         globals: true,
         environment: 'jsdom',
